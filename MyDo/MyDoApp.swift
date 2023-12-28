e8a50117e520161a53e77925263e4995d16ebfa3
hyeondxx@gmail.com <hyeondxx@gmail.com>
2023-12-28 09:05:13 +0900

    Initial Commit

---
+++ MyDo/MyDoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyDoApp.swift
+//  MyDo
+//
+//  Created by 정현 on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MyDoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
