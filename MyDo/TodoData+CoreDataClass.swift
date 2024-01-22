06bd3243c028cd565ac8e9e0096069046cc1c799
hyeondxx@gmail.com <hyeondxx@gmail.com>
2024-01-22 13:25:41 +0900

    feat: CoreData file 등록완료

---
+++ MyDo/TodoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoData+CoreDataClass.swift
+//  MyDo
+//
+//  Created by 정현 on 1/22/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoData)
+public class TodoData: NSManagedObject {
+
+}
